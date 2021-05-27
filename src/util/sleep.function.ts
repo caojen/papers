9a5f58b7ee7dc554a5c4090dfba192c42f4a325e
曹鉴恩 <caojen@mail2.sysu.edu.cn>
2021-05-27 21:38:20 +0800

    debug: sleep default arg

--- src/util/sleep.function.ts
+++ src/util/sleep.function.ts
@@ -2,8 +2,9 @@ import { Config } from 'src/config/config.entity';
 import log from 'src/util/logger.functions';
 
 const config = new Config();
+const interval = config.time.interval;
 
-export function sleep(ms: number = config.time.interval) {
+export function sleep(ms: number = interval) {
   log.warn(['sleeping for', ms, '...']);
   return new Promise((resolve) => {
     setTimeout(resolve, ms);
