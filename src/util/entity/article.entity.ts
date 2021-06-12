c3e62728ae0dec8edd531b34e76105139ccb7e5c
曹鉴恩 <caojen@mail2.sysu.edu.cn>
2021-06-12 16:08:33 +0800

    article: init

--- src/util/entity/article.entity.ts
+++ src/util/entity/article.entity.ts
@@ -17,4 +17,27 @@ export class Article {
     authors: Author[];
     abstract: string;
     keywords: Keyword[];
+
+    context: string;
+
+    constructor(context: string) {
+        this.context = context;
+    }
+
+    /**
+     * Resolve this.context into this.
+     * Return true, if resolving done. This means this.sync should be called later to store this into database.
+     * Return false, meaning that resolve failed, or origin_id exists.
+     */
+    async resolve(): Promise<boolean> {
+        return false;
+    }
+
+    /**
+     * Sync this into database.
+     * Always return true.
+     */
+    async sync(): Promise<boolean> {
+        return true;
+    }
 }
