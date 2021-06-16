c35302049bb2b7b927b034058b30e14ef6fab9d3
caojen <caojen@mail2.sysu.edu.cn>
2021-06-17 00:01:46 +0800

    cron: update to every 2 hours

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   // 启动时自动触发一次任务
   cron_main();
   // 触发定时任务
-  new CronJob('0 0 */3 * * *', cron_main, cron_exit, true);
+  new CronJob('0 0 */2 * * *', cron_main, cron_exit, true);
   // cron_main();
   await app.listen(3000); // 后端接入点
 }
